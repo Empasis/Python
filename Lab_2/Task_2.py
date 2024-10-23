aa16bc17db56aa3ade21a84ead9e1c77cb377955
Empasis <103671432+Empasis@users.noreply.github.com>
2024-10-24 00:11:16 +0300

    Update Task_2.py

--- Lab_2/Task_2.py
+++ Lab_2/Task_2.py
@@ -2,7 +2,7 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-money_capital = 1000
+money_capital = spend - salary
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 for _ in range(months-1):
     spend += spend*increase
